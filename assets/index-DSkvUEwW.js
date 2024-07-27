ae2b7605ae0ec70dacb077f30b25d86474b7eab3
Tumbichi <germanviglietti@gmail.com>
2024-07-27 18:45:03 -0300

    chore: add how in header

---
+++ assets/index-DSkvUEwW.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-U2N9L9V3.js";function i(){return r.jsx("div",{children:r.jsx("h1",{children:"Roadmap"})})}export{i as Roadmap};
---
+++ assets/index-DVhfpuHc.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-U2N9L9V3.js";function n(){return o.jsx("div",{children:o.jsx("h1",{children:"Tokenomics"})})}export{n as Tokenomics};
