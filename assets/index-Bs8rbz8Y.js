0b8ef7795bdbf6c9daf3c7aa9b69634859ccfa0c
Tumbichi <germanviglietti@gmail.com>
2024-08-06 01:00:17 -0300

    chore(Hero): update hero image assets

--- assets/index-Bs8rbz8Y.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-B136TP1b.js";function e(){return r.jsx("div",{children:r.jsx("h1",{children:"DaoFramework"})})}export{e as DaoFramework};
---
+++ assets/index-O_5k-rog.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BXlM4gYD.js";function e(){return r.jsx("div",{children:r.jsx("h1",{children:"DaoFramework"})})}export{e as DaoFramework};
