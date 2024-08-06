34fb89d68f817437f920ac1bb0b82ac79c599706
Tumbichi <germanviglietti@gmail.com>
2024-08-06 00:56:31 -0300

    fix(PDFViewer): in mobile screens

--- assets/index-1fgVO-HR.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-W82j4cg-.js";function e(){return r.jsx("div",{children:r.jsx("h1",{children:"DaoFramework"})})}export{e as DaoFramework};
--- assets/index-BUDaKKlF.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-W82j4cg-.js";function i(){return r.jsx("div",{children:r.jsx("h1",{children:"Roadmap"})})}export{i as Roadmap};
